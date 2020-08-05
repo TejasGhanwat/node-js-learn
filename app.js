40283e93dd531bf555a286eb6f7a853a1509b5bf
Tejas Ghanwat <tejas.aghanwat@gmail.com>
2020-08-04 22:52:42 -0700

    initialized express server

---
+++ app.js
@@ -0,0 +1,11 @@
+const express = require("express");
+const app = express();
+const { getPosts } = require("./routes/post.js");
+
+app.get("/", getPosts);
+
+const port = 8080;
+
+app.listen(port, () => {
+  console.log(`A Node JS API is listening on port: ${port}`);
+});
