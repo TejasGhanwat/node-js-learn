de6b660b61aa3feac6d357f1b543a41d93eb1c6b
Tejas Ghanwat <tejas.aghanwat@gmail.com>
2020-08-05 20:26:07 -0700

    database schema added get post working

--- controllers/controllerPosts.js
+++ controllers/controllerPosts.js
@@ -1,7 +1,25 @@
-const getPosts = (req, res) => {
+const Post = require("../models/modelPost");
+
+exports.getPosts = (req, res) => {
   res.json({
     posts: [{ title: "First Post" }, { title: "Second Post" }],
   });
 };
 
-module.exports = getPosts;
+exports.createPost = (req, res) => {
+  const post = new Post(req.body);
+  // console.log("CREATING POST", req.body);
+  post.save((err, result) => {
+    if (err) {
+      return res.status(400).json({
+        error: err,
+      });
+    }
+    res.status(200).json({
+      post: result,
+    });
+  });
+};
+
+// module.exports = createPost;
+// module.exports = getPosts;
